dfd7d5ebcd618433a76e513afe7d0b278f4bba44
whqtker <chosh2001@naver.com>
2024-07-16 18:59:57 +0900

    implement insert_today_stock_data()

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 초기 설정: https://wikidocs.net/126081#google_vignette 참고
 32bits 가상환경을 사용해야 함. https://www.luck7owl.com/it/python/%ED%8C%8C%EC%9D%B4%EC%8D%AC-venv-%EA%B0%80%EC%83%81%ED%99%98%EA%B2%BD-%EA%B5%AC%EC%B6%95%ED%8C%8C%EC%9D%B4%EC%8D%AC-%EB%B2%84%EC%A0%84-%EC%A7%80%EC%A0%95/ 참고, powershell이 아닌 cmd 환경에서 진행해야 함.
+qt.qpa.plugin: Could not find the Qt platform plugin "windows" in ""
+This application failed to start because no Qt platform plugin could be initialized. Reinstalling the application may fix this problem. 오류 발생 시: https://log-mylife.tistory.com/entry/Could-not-load-the-Qt-platform-plugin-%EB%AC%B8%EC%A0%9C-%ED%95%B4%EA%B2%B0%EB%B2%95
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import time
 import pandas as pd
 import os
 import shutil
+import mysql.connector
 
 # 로그인
 kiwoom = Kiwoom()
@@ -13,3 +14,62 @@ kiwoom.CommConnect()
 kospi = kiwoom.GetCodeListByMarket('0')
 kosdaq = kiwoom.GetCodeListByMarket('10')
 codes = kospi + kosdaq
+
+# MySQL 데이터베이스 연결 정보
+db_config = {
+    'user': 'root',
+    'password': '1234',
+    'host': 'localhost',
+    'port': 3306,
+    'database': 'project'
+}
+
+# MySQL 데이터베이스 연결
+db = mysql.connector.connect(
+    user=db_config['user'],
+    password=db_config['password'],
+    host=db_config['host'],
+    port=db_config['port'],
+    database=db_config['database']
+)
+
+cursor = db.cursor()
+
+def insert_today_stock_data():
+    now = datetime.datetime.now()
+    today = now.strftime("%Y%m%d")
+
+    opt = "opt10081"
+    for i, code in enumerate(codes):
+        print(f"{i}/{len(codes)} {code}")
+        data = kiwoom.block_request(opt,
+                              종목코드=code,
+                              기준일자=today,
+                              수정주가구분=1,
+                              output="주식일봉차트조회",
+                              next=0)
+        
+        print(data)
+
+        # 데이터 전처리: 빈 값('')을 None으로 변환
+        preprocessed_data = []
+        for row in data.itertuples(index=False):
+            preprocessed_row = (code,) + tuple(None if cell == '' else cell for cell in row[1:])
+            preprocessed_data.append(preprocessed_row)
+
+        # INSERT 쿼리 작성
+        sql = "INSERT INTO stock (stock_code, current_price, trading_volume, trading_amount, date, open_price, high_price, low_price, adjusted_price_flag, adjusted_price_ratio, sector, sub_sector, stock_info, adjusted_price_event, previous_close_price) VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)"
+        
+        # 쿼리 실행
+        if preprocessed_data:  # 데이터가 비어있지 않은 경우에만 실행
+            cursor.executemany(sql, preprocessed_data)
+
+        # 변경 사항 커밋
+        db.commit()
+
+        time.sleep(3.6)
+
+if __name__ == "__main__":
+    insert_today_stock_data()
+
+    db.close()
