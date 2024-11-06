46c1dc50d3c4333bf7112d6acd663c33e8eb9db1
whqtker <chosh2001@naver.com>
2024-11-06 11:01:37 +0900

    style: 주석 추가

--- subprocess_wrapper.py
+++ subprocess_wrapper.py
@@ -4,7 +4,7 @@ from telegram_bot import send_message
 
 async def sub_get_model_output():
     await send_message("DB에 모델 출력 삽입 시작.")
-    python_3_12_path = r"C:\Users\chosh\AppData\Local\Programs\Python\Python312\python.exe" 
+    python_3_12_path = r"C:\Users\chosh\AppData\Local\Programs\Python\Python312\python.exe"  # 파이썬 3.12 경로 설정
     script_to_run = os.path.join(os.path.dirname(__file__), "get_model_output.py")  # 스크립트 경로 설정
 
     try:
